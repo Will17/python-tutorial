22af0342520916e94feda9776b0e0aadcb9e04c0
Will17 <williampecoraro98@gmail.com>
2024-05-16 09:26:14 +0100

    Resolved first tutorial to test env

--- packages/mastrogpt/index.py
+++ packages/mastrogpt/index.py
@@ -5,8 +5,10 @@ def main(arg):
     data = {
         "services": [
             ## 1c add the demo entry, an object with name Demo and url mastrogpt/demo
+            {"name":"Demo", "url":"mastrogpt/demo"},
             
             ## 2c add the OpenAI entry, an object with name OpenAI and url openai/chat
+            {"name":"OpenAI","url":"openai/chat"}
         ]
     }
     return {"body": data}
--- packages/openai/chat.py
+++ packages/openai/chat.py
@@ -10,32 +10,50 @@ MODEL = "gpt-35-turbo"
 
 def request(input, role=ROLE):
     ## 3a generate the role as shown in test3.test_request
-    pass
+    system = {"role":"system","content":role}
+    user = {"role":"user","content":input}
+    return [system,user]
 
 def ask(ai, input, role=ROLE):
     
     comp = None
     ## 3b invoke the chat completion API
+    comp = ai.chat.completions.create(model=MODEL, messages=request(input,role))
     
     res = "Sorry, there is an error"
     
     ## 3c read the first message content if any
+    if len(comp.choices) > 0:
+        res = comp.choices[0].message.content
         
     return res
 
 def connect(args):
-    pass
     ## 2a connect ai with Azure OpenAI and return the api object
+    client = AzureOpenAI(
+        api_key=args.get("OPENAI_API_KEY"),
+        azure_endpoint = args.get("OPENAI_API_HOST"),
+        api_version="2023-12-01-preview", 
+    )
+
+    return client
 
 def main(args):
     # connect to the AI
     ai = connect(args)
+
+    #comp = ai.completions.create(model=MODEL, prompt=request("test",ROLE))
 
     # read input and produce output
     input = args.get("input", "")
 
     output = "Connection Error."
     ## 2b retrieve the model we use, check the status and return 'Welcome.' if is 'succeeded'
+    global MODEL
+    model = ai.models.retrieve(MODEL)
+    if model.status == 'succeeded':
+        output = "Welcome."
+    
 
     # if the input is not empty, ask to the AI
     if input != "":
